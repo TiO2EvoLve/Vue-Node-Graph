0e635cd6a25c68767c24c11e2921c7b52891442f
TiO2EvoLve <49835162+TiO2EvoLve@users.noreply.github.com>
2025-08-13 16:01:15 +0800

    Initial commit

---
+++ src/components/NodeEditor.vue
@@ -0,0 +1,21 @@
+﻿<script setup lang="ts">
+
+import { NodeEditor, ClassicPreset } from "rete";
+import type { GetSchemes} from "rete";
+
+
+type Schemes = GetSchemes<ClassicPreset.Node, ClassicPreset.Connection<ClassicPreset.Node, ClassicPreset.Node>>;
+
+const editor = new NodeEditor<Schemes>();
+const socket = new ClassicPreset.Socket("socket");
+
+const nodeA = new ClassicPreset.Node("A");
+nodeA.addControl("a", new ClassicPreset.InputControl("text", {}));
+nodeA.addOutput("a", new ClassicPreset.Output(socket));
+await editor.addNode(nodeA);
+
+</script>
+
+<template>
+  <div ></div>
+</template>
